2f060eca318a5fbd4ac1eaf57f5efa5b83444be9
friendlyHobbit <sitavriend@gmail.com>
2024-05-28 13:41:07 +0200

    Update simulation

--- ttest_simulation.R
+++ ttest_simulation.R
@@ -6,7 +6,7 @@ library(tidyr)
 ################# generate population ######################
 
 # set population characteristics
-population_size <- 1000000
+population_size <- 10000000
 # height
 population_group0_height <- 175
 population_group1_height <- 165
@@ -68,6 +68,8 @@ CI_dance <- function(dep_var, pop_mean, n_i, sample_size) {
     
     # does CI include true mean?
     true_CI <- ifelse(res$conf.int[1] < pop_mean & pop_mean < res$conf.int[2], 1, 0)
+    # is result statistically significant?
+    sig <- ifelse(res$p.value <= 0.05, 1, 0)
 
     # add results to df
     res_df <- data.frame(experiment = i, 
@@ -80,13 +82,14 @@ CI_dance <- function(dep_var, pop_mean, n_i, sample_size) {
                          mean_men = res$estimate[1], 
                          mean_women = res$estimate[2], 
                          mean_diff = res$estimate[1] - res$estimate[2], 
-                         trueCI=true_CI)
+                         trueCI = true_CI,
+                         sig = sig)
     
     # update global results_df
     results_df <<- rbind(results_df, res_df)
     
     # show results
-    print(res)
+    #print(res)
   }
 }
 
@@ -95,18 +98,29 @@ CI_dance <- function(dep_var, pop_mean, n_i, sample_size) {
 
 # This runs the simulation of the experiments
 # enter the dependent variable, population mean, number of experiments and sample size per experiment
-CI_dance("IQ", population_IQ,  100, 20)
+CI_dance("height", population_height,  100, 20)
+
+
+# visualize the dance of the p-values
+results_df$sig <- as.factor(results_df$sig)
+
+p_plot <- ggplot(data=results_df, aes(y=p.value, x=experiment, color=sig)) +
+  geom_hline(yintercept=0.05, linetype="dotted", color="black", linewidth = 1) +
+  geom_point() 
+p_plot
+
+# visualize density of means received from all samples
+ggplot(data=results_df, aes(x=p.value)) + geom_density(adjust=1.5, alpha=.4) 
 
 
 # visualize the dance of the CIs
 results_df$trueCI <- as.factor(results_df$trueCI)
 
-plot <- ggplot(data=results_df, aes(y=mean_diff, x=experiment, color=trueCI)) + 
+ci_plot <- ggplot(data=results_df, aes(y=mean_diff, x=experiment, color=trueCI)) + 
   geom_hline(yintercept=results_df$true_mean, linetype="dotted", color="black", linewidth = 1) +
   geom_errorbar(aes(ymin=conf.low, ymax=conf.high), width=.5) +
   geom_point() 
-print(plot)
-
+print(ci_plot)
 
 # visualize density of means received from all samples
 ggplot(data=results_df, aes(x=mean_diff)) + geom_density(adjust=1.5, alpha=.4) 
